f46fc5aebb12ffb226427afa5042f42209e42f12
Swetha <pamulaswetha@email.com>
2021-11-16 21:06:16 +0100

    Commiting version1

--- WorkerService/obj/Debug/net5.0/WorkerService.AssemblyInfo.cs
+++ WorkerService/obj/Debug/net5.0/WorkerService.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("dotnet-WorkerService-03B674C0-610B-412F-B685-BD6C8103B6A1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
